995f3db8a60adb97e9e017accab96c9675f83c78
WaLoigg <167706159+WaLoigg@users.noreply.github.com>
2025-08-31 12:51:22 +0200

    First commit: project creation and test component creation.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Prova } from './prova/prova';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Prova],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
