b73bf71f2d60b24494e8b1cbf8acb77529aad75e
WaLoigg <167706159+WaLoigg@users.noreply.github.com>
2025-09-01 17:53:30 +0200

    Second commit: feat: First component edited with an Angular Material Card.
    Concepts covered:
    -exported class and .class of scss and html.
    -Implementation of Interfaces' methods in prova.ts to check component's creation and destruction.

--- src/app/prova/prova.ts
+++ src/app/prova/prova.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-prova',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './prova.html',
   styleUrl: './prova.scss'
 })
-export class Prova {
+export class Prova implements OnInit, OnDestroy, AfterContentChecked, AfterContentInit, AfterViewChecked,
+  AfterViewInit, DoCheck {
+
+  constructor() {
+    console.log('Constructor called');
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
+  ngDoCheck(): void {
+    console.log('ngDocheck called');
+  }
 
 }
